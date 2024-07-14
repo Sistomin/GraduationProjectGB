a280a2ccb32866385199dc424cbb2a43af0afb44
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2024-07-14 15:52:10 +0700

    Add graduation project for GB, by IstominSA

---
+++ Pet-Mart_SPRING-BOOT-main/src/main/java/com/online/petstore/controller/AdminController.java
@@ -0,0 +1,204 @@
+package com.online.petstore.controller;
+
+import java.util.List;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.online.petstore.dao.AccountDAO;
+import com.online.petstore.dao.OrderDAO;
+import com.online.petstore.dao.ProductDAO;
+import com.online.petstore.entity.Account;
+import com.online.petstore.entity.Product;
+import com.online.petstore.form.ProductForm;
+import com.online.petstore.form.UserForm;
+import com.online.petstore.model.OrderDetailInfo;
+import com.online.petstore.model.OrderInfo;
+import com.online.petstore.validator.ProductFormValidator;
+import com.online.petstore.validator.UserFormValidator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@Transactional
+public class AdminController {
+	
+	@Autowired
+	private OrderDAO orderDAO;
+	
+   @Autowired
+   private ProductDAO productDAO;
+   
+   @Autowired
+   private AccountDAO accountDAO;
+   
+   @Autowired
+   private ProductFormValidator productFormValidator;
+   
+   @Autowired
+   private UserFormValidator userFormValidator;
+   
+   @InitBinder
+   public void myInitBinder(WebDataBinder dataBinder) {
+	   Object target = dataBinder.getTarget();
+       if (target == null) {
+           return;
+       }
+       System.out.println("Target=" + target);
+
+       if (target.getClass() == ProductForm.class) {
+           dataBinder.setValidator(productFormValidator);
+       } else if (target.getClass() == UserForm.class) {
+           dataBinder.setValidator(userFormValidator);
+       }
+   }
+   
+   @Autowired
+   private BCryptPasswordEncoder passwordEncoder;
+   // GET: страница авторизации
+   @GetMapping({ "/admin/login" })
+   public String login(Model model) {
+
+      return "login";
+   }
+   @GetMapping({ "/admin/signup" })
+   public String signup(Model model) {
+	   model.addAttribute("userForm", new UserForm());
+      return "signup";
+   }
+   
+   @GetMapping({ "/admin/accountInfo" })
+   public String accountInfo(Model model) {
+
+      UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+      System.out.println(userDetails.getPassword());
+      System.out.println(userDetails.getUsername());
+      System.out.println(userDetails.isEnabled());
+
+      model.addAttribute("userDetails", userDetails);
+      return "accountInfo";
+   }
+   
+   @GetMapping({ "/admin/orderList" })
+   public String orderList(Model model,
+         @RequestParam(value = "page", defaultValue = "1") String pageStr) {
+      return "orderList";
+   }
+
+   @GetMapping({ "/admin/order" })
+   public String orderView(Model model, @RequestParam("orderId") String orderId) {
+      OrderInfo orderInfo = null;
+      if (orderId != null) {
+         orderInfo = this.orderDAO.getOrderInfo(orderId);
+      }
+      if (orderInfo == null) {
+         return "redirect:/admin/orderList";
+      }
+      List<OrderDetailInfo> details = this.orderDAO.listOrderDetailInfos(orderId);
+      orderInfo.setDetails(details);
+
+      model.addAttribute("orderInfo", orderInfo);
+
+      return "order";
+   }
+   
+   @PostMapping("/admin/account")
+   public String saveAccount(Model model,
+                             @ModelAttribute("userForm") @Validated UserForm userForm,
+                             BindingResult result,
+                             final RedirectAttributes redirectAttributes) {
+
+	   if (result.hasErrors()) {
+           return "signup"; // Возврат страницы регистрации с ошибками проверки
+       }
+
+       try {
+           Account account = new Account();
+           account.setUserName(userForm.getUsername());
+           account.setEncrytedPassword(passwordEncoder.encode(userForm.getPassword()));
+           account.setUserRole("ROLE_USER"); // Default role
+           accountDAO.saveAccount(account);
+       } catch (Exception e) {
+           String message = "Error occurred during account creation.";
+           model.addAttribute("errorMessage", message);
+           return "signup"; // Возврат страницы регистрации с сообщением об ошибке
+       }
+
+       return "redirect:/admin/login";
+     }
+
+
+
+   
+   // GET: Показать продукты.
+   @GetMapping({ "/admin/product" })
+   public String product(Model model, @RequestParam(value = "code", defaultValue = "") String code) {
+      ProductForm productForm = null;
+
+      if (code != null && code.length() > 0) {
+         Product product = productDAO.findProduct(code);
+         if (product != null) {
+            productForm = new ProductForm(product);
+         }
+      }
+      if (productForm == null) {
+         productForm = new ProductForm();
+         productForm.setNewProduct(true);
+      }
+      model.addAttribute("productForm", productForm);
+      return "product";
+   }
+
+   // POST: Сохранить продукты
+   @PostMapping({ "/admin/product" })
+   public String productSave(Model model,
+         @ModelAttribute("productForm") @Validated ProductForm productForm, //
+         BindingResult result,
+         final RedirectAttributes redirectAttributes) {
+
+      if (result.hasErrors()) {
+         return "product";
+      }
+      try {
+         productDAO.save(productForm);
+      } catch (Exception e) {
+         Throwable rootCause = ExceptionUtils.getRootCause(e);
+         String message = rootCause.getMessage();
+         model.addAttribute("errorMessage", message);
+         // Показать продуктовую форму.
+         return "product";
+      }
+
+      return "redirect:/productList";
+   }
+   
+   
+   @GetMapping({ "/admin/product/delete" })
+   public String deleteProduct(Model model, @ModelAttribute("productForm") ProductForm productForm,
+           final RedirectAttributes redirectAttributes) {
+       try {
+           productDAO.delete(productForm);
+           redirectAttributes.addFlashAttribute("message", "Product deleted successfully!");
+       } catch (Exception e) {
+           Throwable rootCause = ExceptionUtils.getRootCause(e);
+           String message = rootCause.getMessage();
+           model.addAttribute("errorMessage", message);
+       }
+
+       return "redirect:/productList";
+   }
+}
